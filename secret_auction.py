a3abf0ce6046d2745d3ed5b7954579462bcfafda
Michael Primavera <pastasauce59@gmail.com>
2023-02-15 16:55:40 -0500

    added art.py and secret_auction.py with starter code for program.

---
+++ secret_auction.py
@@ -0,0 +1,8 @@
+from art import logo
+print("Welcome to the secret auction. Please prepare to place your bids.")
+print(logo)
+input("What is your name? \n")
+input("And what is your bid? \n$")
+
+import os
+os.system('clear')
