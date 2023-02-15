d5388a2a79a5a5260e53f0e30c4da078f92dddea
Michael Primavera <pastasauce59@gmail.com>
2023-02-15 18:22:35 -0500

    created a function to house the logic for finding the highest bidder and make the code cleaner and easier to read.

--- secret_auction.py
+++ secret_auction.py
@@ -5,6 +5,15 @@ print(logo)
 more_bids = True
 bidders = {}
 
+def find_highest_bidder(bidding_dict):
+    bidder_name = ''
+    highest_bid = 0
+    for bidder in bidding_dict:
+        if bidding_dict[bidder] > highest_bid:
+            bidder_name = bidder
+            highest_bid = bidding_dict[bidder]
+    print(f"The winner is {bidder_name} with a bid of ${highest_bid}!")
+
 while more_bids:
     name = input("What is your name? \n")
     bid = int(input("And what is your bid? \n$"))
@@ -13,16 +22,7 @@ while more_bids:
     contine_ = input("Are there any other bidders? Type \"Yes\" or \"No\" \n")
     if contine_.lower() != "yes":
         more_bids = False
-        
-        bidder_name = ''
-        highest_bid = 0
-        for bidder in bidders:
-            if bidders[bidder] > highest_bid:
-                bidder_name = bidder
-                highest_bid = bidders[bidder]
-
-        print(f"The winner is {bidder_name} with a bid of ${highest_bid}!")
-
+        find_highest_bidder(bidders)
     else:
         #works for VS Code to clear command line. #ymmv
         import os
