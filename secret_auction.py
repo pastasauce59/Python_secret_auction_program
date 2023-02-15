be8df1e85d44ef20f1dd24159fca2a26ff7fdfde
Michael Primavera <pastasauce59@gmail.com>
2023-02-15 17:25:58 -0500

    created 'bidders' dicitionary, added a while loop to captuer user inputs and then add new bidder to 'bidders', created if statement to switch boolean if there are no longer any bidders.

--- secret_auction.py
+++ secret_auction.py
@@ -1,8 +1,21 @@
 from art import logo
 print("Welcome to the secret auction. Please prepare to place your bids.")
 print(logo)
-input("What is your name? \n")
-input("And what is your bid? \n$")
 
-import os
-os.system('clear')
+more_bids = True
+bidders = {}
+
+while more_bids:
+    name = input("What is your name? \n")
+    bid = input("And what is your bid? \n$")
+    bidders[name] = bid
+
+    # print(bidders)
+    contine_ = input("Are there any other bidders? Type \"Yes\" or \"No\" \n")
+    if contine_.lower() != "yes":
+        more_bids = False
+        print(bidders)
+    else:
+        #works for VS Code to clear command line. #ymmv
+        import os
+        os.system('clear')
