a401935c31c0921288ecd16cea57e76c7bfe9515
Michael Primavera <pastasauce59@gmail.com>
2023-02-15 17:56:42 -0500

    changed 'bid' input to be an integer, created logic to capture the highest bid from 'bidders' dictionary with a for-in loop that prints the winners name with their highest bid.

--- secret_auction.py
+++ secret_auction.py
@@ -7,14 +7,22 @@ bidders = {}
 
 while more_bids:
     name = input("What is your name? \n")
-    bid = input("And what is your bid? \n$")
+    bid = int(input("And what is your bid? \n$"))
     bidders[name] = bid
 
-    # print(bidders)
     contine_ = input("Are there any other bidders? Type \"Yes\" or \"No\" \n")
     if contine_.lower() != "yes":
         more_bids = False
-        print(bidders)
+        
+        bidder_name = ''
+        highest_bid = 0
+        for bidder in bidders:
+            if bidders[bidder] > highest_bid:
+                bidder_name = bidder
+                highest_bid = bidders[bidder]
+
+        print(f"The winner is {bidder_name} with a bid of ${highest_bid}!")
+
     else:
         #works for VS Code to clear command line. #ymmv
         import os
